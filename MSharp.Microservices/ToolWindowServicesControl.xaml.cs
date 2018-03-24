4ee1b1c45fc665b19d00e4e5993963be2dd9ab85
mrz.zarif <mrz.zarif@gmail.com>
2018-03-24 13:47:59 +0430

    Open code in VS

--- MSharp.Microservices/ToolWindowServicesControl.xaml.cs
+++ MSharp.Microservices/ToolWindowServicesControl.xaml.cs
@@ -7,6 +7,10 @@
     using System.Windows.Controls;
     using System.Data;
     using System.Linq;
+    using System.Runtime.InteropServices;
+    using EnvDTE;
+    using System;
+    using System.Runtime.InteropServices.ComTypes;
 
     /// <summary>
     /// Interaction logic for ToolWindowServicesControl.
@@ -29,8 +33,103 @@
                 JsonValue value = JsonValue.Parse(text);
                 result = value["profiles"]["Website"]["applicationUrl"].ToString().Replace('"', ' ').Trim();
             }
-            
+
             return result;
+        }
+
+        [DllImport("ole32.dll")]
+        private static extern int CreateBindCtx(uint reserved, out IBindCtx ppbc);
+
+        public static DTE GetDTE(int processId)
+        {
+            string progId = "!VisualStudio.DTE.15.0:" + processId.ToString();
+            object runningObject = null;
+
+            IBindCtx bindCtx = null;
+            IRunningObjectTable rot = null;
+            IEnumMoniker enumMonikers = null;
+
+            try
+            {
+                Marshal.ThrowExceptionForHR(CreateBindCtx(reserved: 0, ppbc: out bindCtx));
+                bindCtx.GetRunningObjectTable(out rot);
+                rot.EnumRunning(out enumMonikers);
+
+                IMoniker[] moniker = new IMoniker[1];
+                IntPtr numberFetched = IntPtr.Zero;
+                while (enumMonikers.Next(1, moniker, numberFetched) == 0)
+                {
+                    IMoniker runningObjectMoniker = moniker[0];
+
+                    string name = null;
+
+                    try
+                    {
+                        if (runningObjectMoniker != null)
+                        {
+                            runningObjectMoniker.GetDisplayName(bindCtx, null, out name);
+                        }
+                    }
+                    catch (UnauthorizedAccessException)
+                    {
+                        // Do nothing, there is something in the ROT that we do not have access to.
+                    }
+
+                    if (!string.IsNullOrEmpty(name) && string.Equals(name, progId, StringComparison.Ordinal))
+                    {
+                        Marshal.ThrowExceptionForHR(rot.GetObject(runningObjectMoniker, out runningObject));
+                        break;
+                    }
+                }
+            }
+            finally
+            {
+                if (enumMonikers != null)
+                {
+                    Marshal.ReleaseComObject(enumMonikers);
+                }
+
+                if (rot != null)
+                {
+                    Marshal.ReleaseComObject(rot);
+                }
+
+                if (bindCtx != null)
+                {
+                    Marshal.ReleaseComObject(bindCtx);
+                }
+            }
+
+            return (DTE)runningObject;
+        }
+        private DTE SolutionDte(string solutionPath)
+        {
+            DTE resultDte = null;
+
+            System.Diagnostics.Process[] processes = System.Diagnostics.Process.GetProcessesByName("devenv");
+            foreach (System.Diagnostics.Process p in processes)
+            {
+                var foundDte = GetDTE(p.Id);
+                if (foundDte != null)
+                {
+                    if (foundDte.Solution.FullName == solutionPath)
+                    {
+                        resultDte = foundDte;
+                    }
+                }
+
+            }
+
+            if (resultDte == null)
+            {
+                Type typeDTE = Type.GetTypeFromProgID("VisualStudio.DTE.15.0");
+                resultDte = (DTE)Activator.CreateInstance(typeDTE, true);
+                resultDte.UserControl = true;
+                resultDte.Solution.Open(solutionPath);
+                //resultDte.MainWindow.Activate();
+                //Marshal.ReleaseComObject(resultDte);
+            }
+            return resultDte;
         }
 
         [SuppressMessage("Microsoft.Globalization", "CA1300:SpecifyMessageBoxOptions", Justification = "Sample code")]
@@ -55,7 +154,7 @@
                 string text = File.ReadAllText(filename);
                 JsonValue value = JsonValue.Parse(text);
                 var solution = value[0]["Solution"]["FullName"].ToString();
-                txtSolutionName.Text = "Solution: " + solution.Replace('"',' ');
+                txtSolutionName.Text = "Solution: " + solution.Replace('"', ' ');
                 var services = value[0]["Services"];
 
                 var arrServices = JsonArray.Parse(services.ToString());
@@ -76,7 +175,7 @@
                         dr["solution_path"] = solutionFile.FullName;
                         var ApplicationUrl = applicationUrl(solutionFile);
                         dr["application_url"] = ApplicationUrl;
-                        dr["port"] = ApplicationUrl.Substring(ApplicationUrl.LastIndexOf(":")+1);
+                        dr["port"] = ApplicationUrl.Substring(ApplicationUrl.LastIndexOf(":") + 1);
                     }
 
                     tblServices.Rows.Add(dr);
@@ -95,7 +194,7 @@
         {
             MessageBox.Show("Button is unchecked.");
         }
-        
+
         private void open_click(object sender, RoutedEventArgs e)
         {
             MenuItem mnuItem = (MenuItem)sender;
@@ -113,20 +212,44 @@
 
         private void code_click(object sender, RoutedEventArgs e)
         {
-            var msg = "Is Nothing";
-
             Button mnuItem = (Button)sender;
             if (mnuItem.CommandParameter != null)
             {
                 string solutionPath = mnuItem.CommandParameter.ToString().Trim();
                 if (solutionPath.Length != 0)
                 {
-                    msg = solutionPath;
-                    //MessageBox.Show(solutionPath);
+                    var resultDte = SolutionDte(solutionPath);
+
+                    //DTE resultDte = null;
+
+                    //System.Diagnostics.Process[] processes = System.Diagnostics.Process.GetProcessesByName("devenv");
+                    //foreach (System.Diagnostics.Process p in processes)
+                    //{
+                    //    var foundDte = GetDTE(p.Id);
+                    //    if (foundDte.Solution.FullName == solutionPath)
+                    //    {
+                    //        resultDte = foundDte;
+                    //        //resultDte.MainWindow.Activate();
+                    //    }
+                    //}
+
+                    //if (resultDte == null)
+                    //{
+                    //    Type typeDTE = Type.GetTypeFromProgID("VisualStudio.DTE.15.0");
+                    //    resultDte = (DTE)Activator.CreateInstance(typeDTE, true);
+                    //    resultDte.UserControl = true;
+                    //    resultDte.Solution.Open(solutionPath);
+                    //    //resultDte.MainWindow.Activate();
+                    //    //Marshal.ReleaseComObject(resultDte);
+                    //}
+
+                    if (resultDte != null)
+                    {
+                        resultDte.MainWindow.Activate();
+
+                    }
                 }
             }
-
-            MessageBox.Show(msg);
         }
 
     }
